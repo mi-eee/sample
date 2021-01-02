8bc6af66157090bc8bf5cf94fc7d6006fefc64d3
mi-eee <74106105+mi-eee@users.noreply.github.com>
2021-01-02 20:38:04 +0900

    Create Untitled-3.js

---
+++ Untitled-3.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(function(){
+    $('#this').click(function(){
+        $('.open').fadeIn();
+    });
+    
+    $('.open').click(function(){
+       $('.open').fadeOut(); 
+    });
+    
+});
